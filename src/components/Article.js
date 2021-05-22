c08c4f3a699e25e31a4f7eb4bdfd7f182a65a850
Rendy Artha <rendyartha312@gmail.com>
2021-05-23 00:12:42 +0700

    fixing button load more

--- src/components/Article.js
+++ src/components/Article.js
@@ -8,28 +8,30 @@ const Article = ({articles}) => {
   }
 
   return (
-    <div className="my-8 lg:mt-24 w-full max-w-6xl mx-auto px-4 md:px-6 xl:px-10 2xl:px-16 flex flex-row flex-wrap justify-between"> 
-      {articles.slice(0, pageLoad).map((article) => {
-        return (
-          <ListArticle 
-            key={article._id} 
-            article = {article}
-            loadMore = {loadMore}
-          />
-        )
-      })}
+    <>
+      <div className="mt-8 lg:mt-24 w-full max-w-6xl mx-auto px-4 md:px-6 xl:px-10 2xl:px-16 flex flex-row flex-wrap justify-between"> 
+        {articles.slice(0, pageLoad).map((article) => {
+          return (
+            <ListArticle 
+              key={article._id} 
+              article = {article}
+              loadMore = {loadMore}
+            />
+          )
+        })}
+      </div>
       {pageLoad > 8 
         ? 
           null
         :
         <button 
-          className="bg-orange text-white shadow-orange mt-2 py-2 mx-auto focus:outline-none rounded w-32"
+          className="bg-orange text-white shadow-orange mb-4 py-2 mx-auto flex justify-center focus:outline-none rounded w-32"
           onClick={loadMore}
         >
           Load More
         </button>
       }
-    </div>
+    </>
   )
 }
 
